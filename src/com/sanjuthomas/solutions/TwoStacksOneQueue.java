8464c66fd86901de01344be1c8f67a1ecb0cd5bf
sanjuthomas <sanju@sanju.org>
2019-11-02 17:57:02 -0400

    two stacks one queue

--- src/com/sanjuthomas/solutions/TwoStacksOneQueue.java
+++ src/com/sanjuthomas/solutions/TwoStacksOneQueue.java
@@ -26,7 +26,10 @@ public class TwoStacksOneQueue {
     while(!s1.isEmpty()) {
       s2.push(s1.pop());
     }
-    return s2.pop();
+    if(!s2.isEmpty()) {
+      return s2.pop();
+    }
+    throw new IllegalStateException("No one is present here!!");
   }
   
   
